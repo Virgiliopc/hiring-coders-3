32a5bc0375b6ae8163e7230d50c502480ce6a49a
Virgílio Pires da Costa <virgilio.pires.costa@gmail.com>
2022-05-15 20:00:40 -0300

    add arrow.js
    "add arrow.js"

---
+++ gama-no-javascript-intermediário/ready/arrow.js
@@ -0,0 +1,17 @@
+function soma(x ,y) {
+  return x + y;
+}
+
+console.log(soma(3,4));
+
+const multiplicacao = function(x, y) {
+  return x * y;
+};
+
+//console.log(multiplicacao(5, 5));
+
+const dividir = (x,y) => {
+  return x / y;
+};
+
+const objeto = () => ({ nome: "JC", sobrenome: "Bombardelli"});
